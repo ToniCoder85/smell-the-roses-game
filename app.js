570a3d60e3789f9b8e5525fd043e0a2af9197746
ToniCoder85 <info@iamtoninah.com>
2024-11-04 09:24:22 -0500

    more styling and adding photo

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
+// use a template literal to say a greeting after player enters name let playerName = "name"; let greeting = `Dawning,`;
+// let greeting = `Dawning, ${playerName}`
+
+// create a function for the input of name and Age.*******
+
+
+let playerName = "";
 let flowerCount = 0;
-document.getElementById(pickFlower).addEventListener('click',() => {
+// greeting to player
+  function greetingToPlayer() {
+    console. log(`Hello Flowerchild ${playerName}`);
+  }
+//   Start Button execution
+    document.getElementById('start button').addEventListener('click',() => {
+        playerName = document.getElementById('Name').value;
+        greetingToPlayer();
+        alert(`Hello Flowerchild ${playerName}! Start picking flowers.`);
+        });
+
+//    greetingToPlayer();
+    // let flowerCount = 0;
+
+// Picking A Flower button event listner
+document.getElementById('pickFlower').addEventListener('click',() => {
     flowerCount++; 
-    document.getElementById('flowerCount').textContent = flowerCount;
+
+ document.getElementById('flowerCount').textContent = flowerCount;
+    if (flowerCount ===12) {
+    document.getElementById('winMessage').style.display = "block";
+}
 });
+
+function askQuestion(){
+    if (flowercount === 5) {
+        let reponse = prompt("Have you  hugged a tree before?");
+        if(Response.toLowercase() === "no") {
+            alert("Go ahead and pick a flower!");
+        }    }
